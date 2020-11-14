d2f4a0e0aa657e2a499051d47756330e2a07214b
Scott Prigmore <prig6030@stthomas.edu>
2020-11-14 17:44:02 -0600

    Red - created predictable turn test

---
+++ SkunkProject/test/skunk/domain/TestPredictableTurn.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableTurn {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
