3550b03cd36365568e64e1c3632988363808224e
Scott Prigmore <prig6030@stthomas.edu>
2020-11-14 16:54:06 -0600

    Red - added failing roll test

--- SkunkProject/test/skunk/domain/TestPredictableDice.java
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -15,4 +15,14 @@ class TestPredictableDice {
 		assertEquals(2, dice.getLastRoll());
 	}
 
+	@Test
+	void test_predictable_dice_246() {
+		Die die1 = new PredictableDie(new int[] {1,2,3});
+		Die die2 = new PredictableDie(new int[] {1,2,3});
+		Dice dice = new Dice(die1, die2);
+		dice.roll();
+
+		assertEquals(3, dice.getLastRoll());
+	}
+
 }
