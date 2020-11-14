91b21dd7a7890fed7d8c83104a07a8d8f59e7911
Scott Prigmore <prig6030@stthomas.edu>
2020-11-14 16:35:16 -0600

    Red - new predictable dice test

---
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDice {
+
+	@Test
+	void test_predictable_dice_constructor() {
+		fail("Not yet implemented");
+	}
+
+}
