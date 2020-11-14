03f0c6308fe246a015b1f4987b8955b7f2af4bf5
Scott Prigmore <prig6030@stthomas.edu>
2020-11-14 14:48:18 -0600

    Red - first predictable die test

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPredictableDie {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
