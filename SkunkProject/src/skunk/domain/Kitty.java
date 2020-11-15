f2d58bf9b338603bb1c7ea855b0fe96fcb6c4dc8
Scott Prigmore <prig6030@stthomas.edu>
2020-11-15 11:18:51 -0600

    Red - Added kitty test

--- SkunkProject/src/skunk/domain/Kitty.java
+++ SkunkProject/src/skunk/domain/Kitty.java
@@ -2,4 +2,18 @@ package skunk.domain;
 
 public class Kitty {
 
+	private int chips;
+	
+	public Kitty() {
+		setKitty(50);
+	}
+	
+	private void setKitty(int chips) {
+		this.chips = chips;
+	}
+
+	public int getKitty() {
+		return this.chips;
+	}
+
 }
