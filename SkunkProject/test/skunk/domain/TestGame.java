14d24c94eac734760d2e7ab3dcd3ccb47137b8f3
Scott Prigmore <prig6030@stthomas.edu>
2020-11-15 14:16:04 -0600

    Red - added game test

---
+++ SkunkProject/test/skunk/domain/TestGame.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGame
+{
+
+	@Test
+	void test_game_constructor()
+	{
+		fail("Not yet implemented");
+	}
+
+}
