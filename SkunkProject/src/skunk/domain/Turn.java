7b7d01e9799bff08f8ec9c9d5b6f2715292c7301
Scott Prigmore <prig6030@stthomas.edu>
2020-11-15 13:56:40 -0600

    Blue - refactored constants

--- SkunkProject/src/skunk/domain/Turn.java
+++ SkunkProject/src/skunk/domain/Turn.java
@@ -1,7 +1,10 @@
 package skunk.domain;
 
 public class Turn {
+	private static final int SKUNK_PENALTY = 1;
+	private static final int SKUNK_DEUCE_PENALTY = 2;
 	private static final int DOUBLE_SKUNK_PENALTY = 4;
+	
 	private Dice dice;
 	private Kitty kitty;
 	private Player player;
@@ -27,18 +30,18 @@ public class Turn {
 		this.state = dice.getState();
 		if(this.state == DiceState.DOUBLE_SKUNK) {
 			this.kitty.setKitty(this.kitty.getKitty() + DOUBLE_SKUNK_PENALTY); 
-			this.player.setChips(this.player.getChips() - 4);
+			this.player.setChips(this.player.getChips() - DOUBLE_SKUNK_PENALTY);
 			this.player.setGamePoints(0);
 			this.player.setTurnPoints(0);
 		}
 		else if (this.state == DiceState.SKUNK_DEUCE) {
-			this.kitty.setKitty(this.kitty.getKitty() + 2); 
-			this.player.setChips(this.player.getChips() - 2);
+			this.kitty.setKitty(this.kitty.getKitty() + SKUNK_DEUCE_PENALTY); 
+			this.player.setChips(this.player.getChips() - SKUNK_DEUCE_PENALTY);
 			this.player.setTurnPoints(0);
 		}
 		else if (this.state == DiceState.SKUNK) {
-			this.kitty.setKitty(this.kitty.getKitty() + 1); 
-			this.player.setChips(this.player.getChips() - 1);
+			this.kitty.setKitty(this.kitty.getKitty() + SKUNK_PENALTY); 
+			this.player.setChips(this.player.getChips() - SKUNK_PENALTY);
 			this.player.setTurnPoints(0);
 		}
 		else {
@@ -46,5 +49,4 @@ public class Turn {
 		}
 		return this.player.getTurnPoints();
 	}
-
 }
