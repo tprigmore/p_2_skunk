a49a1d1bdcd451fe7161988ba6418daaed0501e9
Scott Prigmore <prig6030@stthomas.edu>
2020-11-18 17:14:00 -0600

    Red - first failing controller test

---
+++ SkunkProject/test/skunk/domain/TestController.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestController
+{
+
+	@Test
+	void test_controller_constructor()
+	{
+		fail("Not yet implemented");
+	}
+
+}
