fa85d8d518528e99c68e0c13500170957a23c652
Scott Prigmore <prig6030@stthomas.edu>
2020-11-20 16:37:09 -0600

    Red - first failing to string test

--- SkunkProject/test/skunk/domain/TestRandomDie.java
+++ SkunkProject/test/skunk/domain/TestRandomDie.java
@@ -10,13 +10,24 @@ public class TestRandomDie
 	@Test
 	public void test_die_constructor()
 	{
-		boolean flag = false;
+		boolean result = false;
 		Die die = new RandomDie();
 		int lastRoll = die.getLastRoll();
-		if (lastRoll >= 2 && lastRoll <=12)
-			flag = true;
+		if (lastRoll >= 2 && lastRoll <=6)
+			result = true;
 		
-		assertTrue(flag);
+		assertTrue(result);
+	}
+
+	@Test
+	public void to_string()
+	{
+		boolean result = false;
+		Die die = new RandomDie();
+		String string = die.toString();
+		if (string.equals("Die: ") )//|| string.equals("Die: 2") || string.equals("Die: 3") ||  string.equals("Die: 4") || string.equals("Die: 5") || string.equals("Die: 6"))
+			result = true ;
+		assertTrue(result);
 	}
 
 }
