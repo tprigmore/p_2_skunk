f89687450710f012666a26131b11de423c4a9e07
Scott Prigmore <prig6030@stthomas.edu>
2020-11-19 20:28:34 -0600

    Initial skunkApp

---
+++ SkunkProject/src/skunk/domain/SkunkApp.java
@@ -0,0 +1,28 @@
+package skunk.domain;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkApp
+{
+
+	public static void main(String[] args)
+	{
+		Controller controller = new Controller();
+		String response;
+		Dice dice = Dice.getInstance();
+
+		Die die1 = new DieReal();
+		Die die2 = new DieReal();
+		dice.setupDie(die1, die2);
+
+		
+		while(controller.getState() != ControllerState.GAME_OVER) {
+			StdOut.println(controller.getMessage());
+			response = StdIn.readLine();
+			controller.setResponse(response);
+		}
+
+	}
+
+}
